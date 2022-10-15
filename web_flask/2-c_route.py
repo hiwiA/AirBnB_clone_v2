e5f1139b9e8338416ff2b264e3f4b43cf97675e0
Hiwot Ameha <hiwotameha23@gmail.com>
2022-10-15 21:39:12 +0300

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,29 @@
+
 #!/usr/bin/python3
+# starts flask with c thing
 from flask import Flask
-from markupsafe import escape
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    ''' display “Hello HBNB!" '''
+def hello_world():
+    # prints hello
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ define route /hbnb and return a text"""
+    # prints hbnb
     return 'HBNB'
 
 
-@app.route('/c/<text>',  strict_slashes=False)
-def c(text):
-    # show the text after slash
-    return 'C %s' % escape(text.replace("_", " "))
+@app.route('/c/<text>')
+def ctext(text):
+    # prints c with input
+    return 'C ' + text.replace("_", ' ')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
